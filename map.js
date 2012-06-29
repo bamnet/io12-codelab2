03641ae365528771dcd1328e8587a2a358be0249
Brian Michalski <bmichalski@google.com>
2012-06-29 16:00:16 -0700

    No tabs, formatting clean up.

--- map.js
+++ map.js
@@ -1,37 +1,36 @@
+// Global variabl to hold the map
 var map;
+
 function initialize() {
- /**
- * Map options
- */
-var myOptions = {
-     center: new google.maps.LatLng(0, 0),
-     zoom: 3,
-     minZoom: 1,
-     maxZoom: 19,
-	 mapTypeId: google.maps.MapTypeId.ROADMAP,
-	 scaleControl: true
-};
+  /**
+   * Map options
+   */
+  var myOptions = {
+    center: new google.maps.LatLng(0, 0),
+    zoom: 3,
+    minZoom: 1,
+    maxZoom: 19,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    scaleControl: true
+  };
 
-/*  Initialize map  */
-map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-     
-loadJSONP();
+  /*  Initialize map  */
+  map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 
+  // Load the JsonP data
+  loadJSONP();
 }
 
 function setMapBounds(bounds){
- 
-   var southWestlat = bounds.southWest.lat;
-   var southWestlng = bounds.southWest.lng;
-   var northEastlat = bounds.northEast.lat;
-   var northEastlng = bounds.northEast.lng;
- 
-   var southWest = new google.maps.LatLng(southWestlat,southWestlng);
-   var northEast = new google.maps.LatLng(northEastlat,northEastlng);
-   var bounds = new google.maps.LatLngBounds(southWest,northEast);
-   map.fitBounds(bounds);
+  var southWestlat = bounds.southWest.lat;
+  var southWestlng = bounds.southWest.lng;
+  var northEastlat = bounds.northEast.lat;
+  var northEastlng = bounds.northEast.lng;
 
- }
-
+  var southWest = new google.maps.LatLng(southWestlat,southWestlng);
+  var northEast = new google.maps.LatLng(northEastlat,northEastlng);
+  var bounds = new google.maps.LatLngBounds(southWest,northEast);
+  map.fitBounds(bounds);
+}
 
 google.maps.event.addDomListener(window, 'load', initialize);
--- mapsengine.js
+++ mapsengine.js
@@ -1,53 +1,32 @@
+// Get MapRoot JSON from Maps Engine Directory
 function loadJSONP() {
-    
- // Get MapRoot JSON from Maps Engine Directory
+  // URL of External Script (Step 1)
 
-// URL of External Script (Step 1)
-///////////////////////////////////
-// PASTE STEP 1 BELOW HERE //////
-////////////////////////////////
+  // Create Insertable Script (Step 2)
 
-
-// Create Insertable Script (Step 2)
-///////////////////////////////////
-// PASTE STEP 2 BELOW HERE //////
-////////////////////////////////
-
-
-// load the script (Step 3)
-///////////////////////////////////
-// PASTE STEP 3 BELOW HERE //////
-////////////////////////////////
-   
-    
-}    
+  // Load the script (Step 3)
+}
 
 
 function parseMapRoot(data) {
-    $("#side_bar").append('<div id="map_title"><h3>' + data.name +'</h3></div>');
-    $("#side_bar").append('<ul id="layer_picker" style="list-style-type: none;"></ul>');
-    var index = 0
-    setMapBounds(data.bounds);
-    //Loop Through All The Layers of the Map, and Create MapDataLayers For Each One
-    $.each(data.layers, function(index) {
-        $("#layer_picker").append('<li><input type="checkbox" id="' + index + '" class="layer"/><label for="' + index + '">' + data.layers[index].layerName.replace("Frederick County Virginia", "").replace("County Mosaic", "") + '</label></li>');
-        
-        // Create the MapDataLayer, and Add it To An Array (Step 4)
-        ///////////////////////////////////
-        // PASTE STEP 4 BELOW HERE //////
-        ////////////////////////////////
-        
-        
-    });  
-  
-  //add a JQuery Listener to the new "layer" class, so when a checkbox is checked, it will toggle a layer on or off
-  $('.layer').change(function(){
-  var layerID = parseInt($(this).attr('id'));
-      if ($(this).attr('checked')){
-          gebLayers[layerID].setMap(map);
-      }else{
-          gebLayers[layerID].setMap(null);
-      }      
-});  
-    
+  $("#side_bar").append('<div id="map_title"><h3>' + data.name +'</h3></div>');
+  $("#side_bar").append('<ul id="layer_picker" style="list-style-type: none;"></ul>');
+
+  setMapBounds(data.bounds);
+
+  //Loop through all the layers of the map, and create MapDataLayers for each one
+  $.each(data.layers, function(index) {
+    $("#layer_picker").append('<li><input type="checkbox" id="' + index + '" class="layer"/><label for="' + index + '">' + data.layers[index].layerName.replace("Frederick County Virginia", "").replace("County Mosaic", "") + '</label></li>');
+    // Create the MapDataLayer, and add it to an array (Step 4)
+  });
+
+  //Add a JQuery listener to each layer's checkbox, so when it ticked it will toggle the layer's visiblility.
+  $('.layer').change(function() {
+    var layerID = parseInt($(this).attr('id'));
+    if ($(this).attr('checked')){
+      gebLayers[layerID].setMap(map);
+    } else {
+      gebLayers[layerID].setMap(null);
+    }
+  });
 }
